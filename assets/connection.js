53ae5d8f6a4c8e0f07bfe1f94040a90b7af4ca96
Ebad Rehman <Ebadrehman92@gmail.com>
2021-07-20 16:51:31 -0400

    adding content

---
+++ assets/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Port Connection
+  port: 3306,
+  // Your username
+  user: "root",
+  // Your password
+  password: "Bootcamp123@@",
+  database: "employee_DB",
+});
+
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+});
+
