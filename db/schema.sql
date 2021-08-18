3d4ffe0fa9ef234d781c909e0577679c3cd093f5
Ebad Rehman <Ebadrehman92@gmail.com>
2021-08-18 14:39:37 -0400

    added content everything fixed

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,23 @@
-DROP DATABASE IF EXISTS employee_DB;
-
-CREATE DATABASE employee_DB;
-
-USE employee_DB;
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+USE employee_trackerDB;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  dept VARCHAR(45) NULL,
+  name VARCHAR(45) NULL,
   PRIMARY KEY (id)
 );
-
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(45) NULL,
-  salary DECIMAL NOT NULL,
-  dept_id INT REFERENCES department.id,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
   PRIMARY KEY (id)
 );
-
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(45) NULL,
   last_name VARCHAR(45) NULL,
-  role_id INT REFERENCES role.id,
-  manager_id INT,
+  role_id INT NULL,
+  manager_id INT NULL,
   PRIMARY KEY (id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,21 +1,24 @@
--- department table
-INSERT INTO department (dept) VALUES ('sales');
-INSERT INTO department (dept) VALUES ('engineering');
-INSERT INTO department (dept) VALUES ('finance');
-INSERT INTO department (dept) VALUES ('legal');
-
--- role table
-INSERT INTO role (title, salary, dept_id) VALUES ('IT', 90000, 1);
-INSERT INTO role (title, salary, dept_id)  VALUES ('Intern', 90000, 4);
-INSERT INTO role (title, salary, dept_id)  VALUES ('Engineer', 90000, 2);
-INSERT INTO role (title, salary, dept_id)  VALUES ('Accountant', 85000, 3);
-
--- employee table
-INSERT INTO employee (first_name, last_name, role_id) values ('Jane', 'Austen', 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Mark', 'Twain', 2, 1);
-INSERT INTO employee (first_name, last_name, role_id) values ('Lewis', 'Carroll', 3);
-INSERT INTO employee (first_name, last_name, role_id) values ('Andre', 'Asselin', 4);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Jill', 'Tchaikovsky', 4, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Debbie', 'Smith', 3, 1);
-INSERT INTO employee (first_name, last_name, role_id) values ('Thomas', 'Motorola', 2);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ('Darcy', 'Antone', 1, 1);
+USE employee_trackerDB;
+INSERT INTO department (name)
+VALUES ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000, 2),
+    ("Accountant", 125000, 3),
+    ("Legal Team Lead", 250000, 4),
+    ("Lawyer", 190000, 4);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Oscar", "Wilde", 1),
+    ("James", "Joyce", 2),
+    ("Clarice", "Lispector", 3),
+    ("Jane", "Austen", 4),
+    ("George", "Orwell", 5),
+    ("Bell", "Hooks", 6),
+    ("Charles", "Dickens", 7),
+    ("Kurt", "Vonnegut", 5),
+    ("Maya", "Angelou", 4);
